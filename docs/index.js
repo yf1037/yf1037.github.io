fde657b3d938e3defd92e36b83cc3ba9e733b05b
shawn <ibshawn1@gmail.com>
2023-06-13 12:09:00 -0400

    Header and Footer files read in through javascript file
    Just need to include the javascript file at end of body tag

---
+++ docs/index.js
@@ -0,0 +1,22 @@
+async function includeHTML(element, file) {
+  let header = document.getElementsByTagName(element)[0];
+  if( !header ) {
+    console.error('Element ' + element + ' does not exist');
+  } 
+  await fetch(file)
+  .then((resp) => {
+    if(resp.ok)
+      return resp.text();
+    throw new Error();
+  })
+  .then((resp) => {
+    header.innerHTML = resp;
+  }).catch(() => {
+    console.error('Could not load file: ' + file);
+  });
+}
+
+const headerFile = "header.html";
+const footerFile = "footer.html";
+includeHTML('header', headerFile);
+includeHTML('footer', footerFile);
