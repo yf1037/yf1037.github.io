a451d6b356801acd0e0b7404fb369fc39f057b65
shawn <ibshawn1@gmail.com>
2023-06-13 18:34:36 -0400

    using library for i18n static files, now using npm for building

--- docs/index.js
+++ docs/index.js
@@ -18,44 +18,13 @@ async function includeHTML(element, file) {
 
 
 //i18n below
-
-const languageCode = {
-  "EN": 0,
-  "CN": 1
-}
-
-var lang = "EN";
-var langCode = languageCode[lang];
-
 function setLanguage(language) {
-  lang = language;
-  langCode = languageCode[lang];
-
-  let elements = document.querySelectorAll('[data-l18n]');
-  for(let idx = 0; idx < elements.length; idx = idx + 1) {
-    let element = elements[idx];
-    element.textContent = getI18n(element.getAttribute('data-l18n'));
+  if(language === "EN") {
+    document.location.href="/";
   }
-}
-
-// text: l18n-value: [ENtext, CNtext]
-const i18n = {
-  "VENUE_INFO": ["VENUE INFO", "会场信息"],
-  "ACCOMMODATION":["Accomodation", "住宿"],
-  "GALLERY":["GALLERY", "照片墙"],
-  "REGISTRY":["REGISTRY", "礼物单"],
-  "CONGRATS":["CONGRATS", "贺词"],
-  "Our Wedding":["Our Wedding", "婚礼"],
-  "Date: MM/DD/YYY":["Date: MM/DD/YYYY", "时间：XXXX年XX月XX日"],
-  "More info": ["More info >", "更多信息 >"],
-  "Top": ["Top", "顶部"],
-  "More to come": ["MOre to come...", "实时更新中。。。"],
-  "Under construction": ["Under construction...", "建设中。。。"],
-  "Direction": ["Direction:", "出行指南："]
-};
-
-function getI18n( text ) {
-  return i18n[text][langCode];
+  else {
+    document.location.href="/" + language;
+  }
 }
 
 
@@ -66,8 +35,7 @@ const footerFile = "footer.html";
 includeHTML('head'  , headFile);
 includeHTML('footer', footerFile);
 includeHTML('header', headerFile).then(() => {
-    document.getElementById('languageSelector').addEventListener("click", (event) => {
+    document.getElementById('languageSelector').addEventListener("change", (event) => {
       setLanguage(event.target.value)
   });
-  setLanguage('EN');
 });
