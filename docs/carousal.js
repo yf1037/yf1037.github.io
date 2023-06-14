3a4ceda6ed8efb339e407a92b4cd638049d1a781
Yi <yf1037@nyu.edu>
2023-06-14 18:50:13 -0400

    Added carousal, to be debugged

---
+++ docs/carousal.js
@@ -0,0 +1,30 @@
+import Splide from 'splide';
+
+document.addEventListener( 'DOMContentLoaded', function () {
+  var main = new Splide( '#main-carousel', {
+    type      : 'fade',
+    rewind    : true,
+    pagination: false,
+    arrows    : false,
+  } );
+  
+  var thumbnails = new Splide( '#thumbnail-carousel', {
+    fixedWidth  : 100,
+    fixedHeight : 60,
+    gap         : 10,
+    rewind      : true,
+    pagination  : false,
+    isNavigation: true,
+    breakpoints : {
+      600: {
+        fixedWidth : 60,
+        fixedHeight: 44,
+      },
+    },
+  } ).mount();
+} );
+
+main.sync( thumbnails );
+main.mount();
+thumbnails.mount();
+
