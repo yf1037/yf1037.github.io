902f47b4a0ac492ba7a12f9810a36cfbcd74c8b1
Yi <yf1037@nyu.edu>
2023-06-16 13:33:02 -0400

    Added 2nd carousal
    doesn't work for cn??

--- src/carousal.js
+++ src/carousal.js
@@ -1,7 +1,27 @@
-// import Splide from './libs/splide-4.1.3';
+function createCarouselHTML(tagName, name, images) {
+  let section = document.getElementById(tagName);
+  if( !section ) {
+    console.error('tag ' + tagName + " not found for createCarouselHTML");
+    return;
+  }
 
-document.addEventListener( 'DOMContentLoaded', function () {
-  var main = new Splide( '#main-carousel', {
+  let innerHTML = "";
+  innerHTML += '<section id="main-' + name + '" class="splide"><div class="splide__track"><ul class="splide__list">';
+  for( let idx = 0; idx < images.length; idx = idx + 1) {
+    innerHTML += '<li class="splide__slide"><img src="' + images[idx] + '" alt=""></li>';
+  }
+  innerHTML += '</ul></div></section>';
+  innerHTML += '<section id="thumbnail-' + name + '" class="splide"><div class="splide__track"><ul class="splide__list">';
+  for( let idx = 0; idx < images.length; idx = idx + 1) {
+    innerHTML += '<li class="splide__slide"><img src="' + images[idx] + '" alt=""></li>';
+  }
+  innerHTML += '</ul></div></section>';
+
+  section.innerHTML += innerHTML;
+}
+
+function createCarouselJS(name) {
+  var main = new Splide( '#main-' + name, {
     Width: '100%',
     height: "30vw",
     type      : 'fade',
@@ -10,7 +30,7 @@ document.addEventListener( 'DOMContentLoaded', function () {
     arrows    : true,
   } );
 
-  var thumbnails = new Splide( '#thumbnail-carousel', {
+  var thumbnails = new Splide( '#thumbnail-' + name, {
     fixedWidth  : 125,
     fixedHeight : 80,
     gap         : 10,
@@ -29,5 +49,54 @@ document.addEventListener( 'DOMContentLoaded', function () {
   main.sync( thumbnails );
   main.mount();
   thumbnails.mount();
-} );
+};
 
+var imageList = [
+  [
+    'media/st_patricks_day/Proof 038.jpg',
+    'media/st_patricks_day/Proof 044.jpg',
+    'media/st_patricks_day/Proof 048.jpg',
+    'media/st_patricks_day/Proof 101.jpg',
+    'media/st_patricks_day/Proof 106.jpg',
+    'media/st_patricks_day/Proof 131.jpg',
+    'media/st_patricks_day/Proof 149.jpg',
+    'media/st_patricks_day/Proof 034.jpg',
+    'media/st_patricks_day/Proof 164.jpg',
+    'media/st_patricks_day/Proof 019.jpg'
+  ],
+  [
+    'media/events/IMG_6878.jpg',
+    'media/events/IMG_6026.jpg',
+    'media/events/IMG_5822.jpg',
+    'media/events/IMG_6877.jpg',
+    'media/events/IMG_6317.jpg',
+    'media/events/WeChat Image_20230602104135.jpg',
+    'media/events/WeChat Image_20230602104112.jpg',
+    'media/events/IMG_E6453.jpg',
+    'media/events/IMG_6755.jpg',
+    'media/events/MVAEE6641.jpg',
+    'media/events/IMG_8358.jpg',
+    'media/events/IMG_6961.jpg',
+    'media/events/IMG_6918.jpg',
+    'media/events/WeChat Image_20230602172222.jpg',
+    'media/events/WeChat Image_20230602172239.jpg',
+    'media/events/WeChat Image_20230531220409.jpg',
+    'media/events/WeChat Image_20230531220338.jpg',
+    'media/events/IMG_9473.jpg',
+    'media/events/DYXA0340.jpg',
+    'media/events/OROG4664.jpg',
+    'media/events/FFQE4978.jpg',  
+    'media/events/IMG_3318.jpg'
+  ]
+];
+
+function createCarousels(num) {
+  for(var idx = 0; idx < num; idx = idx + 1) {
+    let name = 'carousel' + idx;
+    let tagName = 'car' + idx;
+    createCarouselHTML(tagName, name, imageList[idx]);
+    createCarouselJS(name);
+  }
+}
+
+document.addEventListener( 'DOMContentLoaded', createCarousels(2) );
