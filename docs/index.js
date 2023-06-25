c65310a2aa57ac5af27c8a25d22d209e120adbd4
Yi <yf1037@nyu.edu>
2023-06-25 07:48:01 -0400

    Add animation with bugs

--- docs/index.js
+++ docs/index.js
@@ -56,3 +56,16 @@ includeHTML('header', headerFile).then(() => {
       }
   });
 });
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } 
+  }
+});
