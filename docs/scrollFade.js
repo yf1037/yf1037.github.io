82093f089475d4bb313cb0cddee77dd6130fede5
shawn <ibshawn1@gmail.com>
2023-07-15 21:01:10 -0400

    Fixed fade on a block of text and changed tag to fadeIn

--- docs/scrollFade.js
+++ docs/scrollFade.js
@@ -3,7 +3,7 @@ document.addEventListener("scroll", () => {
   const VIEW_PORT_OFFSET = 0;
   const pageBottom       = (window.innerHeight || document.documentElement.clientHeight) + VIEW_PORT_OFFSET;
 
-  let toFadeList = document.getElementsByClassName('tag');
+  let toFadeList = document.getElementsByClassName('fadeIn');
   for( let idx = 0; idx < toFadeList.length; ++idx ) {
     let element = toFadeList[idx];
     const elementTop = element.getBoundingClientRect().top;
