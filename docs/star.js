a45c8fd5515bc17d7dd2e19c497e292e07661687
Yi <yf1037@nyu.edu>
2023-07-16 22:58:45 -0400

    fix bug with star image in cn page

--- docs/star.js
+++ docs/star.js
@@ -7,14 +7,14 @@ function generatestar(num){
         leftpos = Math.random()*90; //aviod exceed the edge of the page
         toppos = Math.random()*80;
         size = Math.random()*70 + 30;
-        innerHTML += '<div class="star" style="position: absolute;left:'+ leftpos +'%;top: '+ toppos +'%;"><img style = "width: '+ size +'px" src="media/star.png" alt="star image"></div>';
+        innerHTML += '<div class="star" style="position: absolute;left:'+ leftpos +'%;top: '+ toppos +'%;"><img style = "width: '+ size +'px" src="/media/star.png" alt="star image"></div>';
     }
 
     for (let idx = 0; idx < num/2; idx = idx + 1){
         rightpos = Math.random()*90; //aviod exceed the edge of the page
         toppos = Math.random()*80;
         size = Math.random()*70 + 30;
-        innerHTML += '<div class="star" style="position: absolute;right:'+ rightpos +'%;top: '+ toppos +'%;"><img style = "width: '+ size +'px" src="media/star.png" alt="star image"></div>';
+        innerHTML += '<div class="star" style="position: absolute;right:'+ rightpos +'%;top: '+ toppos +'%;"><img style = "width: '+ size +'px" src="/media/star.png" alt="star image"></div>';
     }
     
     section.innerHTML += innerHTML;
